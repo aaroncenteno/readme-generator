575c7c7905b227c62cfdf789c68737fbf0b2f619
ASC8 <asamcent@gmail.com>
2020-10-03 18:58:30 -0700

    generate badge functions

--- index.js
+++ index.js
@@ -72,6 +72,7 @@ return inquirer.prompt(questions);
 
 // function to write README file
 function writeToFile(fileName, data) {
+    
 }
 
 // function to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,28 @@
+const generateList = commalist => {
+    listarray = commalist.split(",")
+    listarray = listarray.map(listitem => "*" + listitem)
+    return listarray.join(" \n")
+};
+
+const generateBadges = badgeArr => {
+    badgeArr = badgeArr.map(itemBadge => {
+        if (itemBadge !== "None") {
+            return "![License](https://img.shields.io/static/v1?label=License&message=" + itemBadge + "&color=BLUE)"
+        }
+    })
+    return badgeArr.join(" ")
+}
+
+
+const generateObjectLicense = objectLicense => {
+    objectLicense = objectLicense.map(itemLicense => "*" + itemLicense)
+    return objectLicense.join(" \n")
+}
+
+
 // function to generate markdown for README
 function generateMarkdown(data) {
+    const {title, description, installation, usage, license, contributing, test, username, email} = data
     return `# ${data.title}
   
   `;
